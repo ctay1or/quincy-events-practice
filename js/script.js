cc86dd33594768738f48c4a629d3a34107fea612
ctaylor <hi.ctaylor.co@gmail.com>
2024-05-31 15:01:56 -0500

    Added JS for keydown and change events

---
+++ js/script.js
@@ -0,0 +1,29 @@
+var body = document.querySelector("body");
+
+//Theme changes between light or dark mode based on user pushing "l" or "d".
+document.addEventListener("keydown", function (e) {
+  console.log(e);
+  if (e.key === "l") {
+    body.classList.add("light");
+  } else if (e.key === "d") {
+    if (body.classList.contains("light")) {
+      body.classList.remove("light");
+    }
+  }
+});
+
+var fave = document.querySelector("#favorite");
+var heading = document.querySelector("h1");
+var selection = "regular";
+
+//Quincy's name changes depending on drop down selection.
+fave.addEventListener("change", function (e) {
+  selection = e.target.value;
+  if (selection === "stealth") {
+    heading.innerText = "Stealth Quincy 😎";
+  } else if (selection === "party") {
+    heading.innerText = "Party Quincy 🥳";
+  } else {
+    heading.innerText = "Quincy";
+  }
+});
